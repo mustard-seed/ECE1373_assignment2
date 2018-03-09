a4a1e6e30fb0ce90ce59d7be3b6baddf9ca7a25e
James Liu <james2006roger@ayoo.ca>
2018-03-09 18:11:00 -0500

    Added comments in create_pr_0.tcl. Added table showing the resources left on the FPGA

--- 8v3_shell/create_pr2_0.tcl
+++ 8v3_shell/create_pr2_0.tcl
@@ -2,7 +2,7 @@
 set projName pr_region_test_proj
 
 #Open the project, regardless of whether it exists
-create_project pr_region 8v3_shell/$projName -part xcvu095-ffvc1517-2-e
+create_project -f pr_region 8v3_shell/$projName -part xcvu095-ffvc1517-2-e
 #Add project ip directories. Change for other projects!
 set_property  ip_repo_paths  {8v3_shell/ocl_ips hls_proj/conv_proj/solution1/impl/ip hls_proj/fc_proj/solution1/impl/ip } [current_project]
 update_ip_catalog
