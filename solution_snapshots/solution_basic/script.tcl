c29c0aefd06737263d0a6652075dfbdb56a6f49d
James Liu <james2006roger@ayoo.ca>
2018-03-15 23:12:25 -0400

    HLS synthesis results before C++ array code change

--- solution_snapshots/solution_basic/script.tcl
+++ solution_snapshots/solution_basic/script.tcl
@@ -8,8 +8,8 @@ set_top accelerator
 add_files ../vgg_16/accelerator/accelerator.cpp -cflags "-I../vgg_16 -DHW_ONLY -std=c++0x"
 add_files -tb ../data
 add_files -tb ../vgg_16/shared/shared.cpp -cflags "-I../vgg_16 -std=c++0x"
-add_files -tb ../vgg_16/accelerator/test/accelerator_test.cpp -cflags "-I../vgg_16 -std=c++0x"
-add_files -tb ../vgg_16/accelerator/accelerator.cpp -cflags "-I../vgg_16 -std=c++0x"
+add_files -tb ../vgg_16/accelerator/test/accelerator_test.cpp -cflags "-DHW_ONLY -I../vgg_16 -std=c++0x"
+add_files -tb ../vgg_16/accelerator/accelerator.cpp -cflags "-DHW_ONLY -I../vgg_16 -std=c++0x"
 open_solution "solution_basic"
 set_part {xcvu095-ffvc1517-2-e} -tool vivado
 create_clock -period 5 -name default
--- solution_snapshots/solution_partitionAndParallel/script.tcl
+++ solution_snapshots/solution_partitionAndParallel/script.tcl
@@ -8,11 +8,12 @@ set_top accelerator
 add_files ../vgg_16/accelerator/accelerator.cpp -cflags "-I../vgg_16 -DHW_ONLY -std=c++0x"
 add_files -tb ../data
 add_files -tb ../vgg_16/shared/shared.cpp -cflags "-I../vgg_16 -std=c++0x"
-add_files -tb ../vgg_16/accelerator/test/accelerator_test.cpp -cflags "-I../vgg_16 -std=c++0x"
-add_files -tb ../vgg_16/accelerator/accelerator.cpp -cflags "-I../vgg_16 -std=c++0x"
+add_files -tb ../vgg_16/accelerator/test/accelerator_test.cpp -cflags "-DHW_ONLY -I../vgg_16 -std=c++0x"
+add_files -tb ../vgg_16/accelerator/accelerator.cpp -cflags "-DHW_ONLY -I../vgg_16 -std=c++0x"
 open_solution "solution_partitionAndParallel"
 set_part {xcvu095-ffvc1517-2-e}
 create_clock -period 5 -name default
+config_compile -name_max_length 30 -pipeline_loops 0 -unsafe_math_optimizations
 source "./accelerator/solution_partitionAndParallel/directives.tcl"
 csim_design -clean -compiler gcc -setup
 csynth_design
