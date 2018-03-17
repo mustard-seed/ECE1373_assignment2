2e8ac9f66827f418a02c57ae035d4bc9d78bf574
James <james2006roger@yahoo.ca>
2018-03-17 21:19:32 +0000

    Updated scripts to create accelerator region

---
+++ accelerator_bd.tcl
@@ -0,0 +1,323 @@
+
+################################################################
+# This is a generated script based on design: pr_region_2
+#
+# Though there are limitations about the generated script,
+# the main purpose of this utility is to make learning
+# IP Integrator Tcl commands easier.
+################################################################
+
+namespace eval _tcl {
+proc get_script_folder {} {
+   set script_path [file normalize [info script]]
+   set script_folder [file dirname $script_path]
+   return $script_folder
+}
+}
+variable script_folder
+set script_folder [_tcl::get_script_folder]
+
+################################################################
+# Check if script is running in correct Vivado version.
+################################################################
+set scripts_vivado_version 2017.2
+set current_vivado_version [version -short]
+
+if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
+   puts ""
+   catch {common::send_msg_id "BD_TCL-109" "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
+
+   return 1
+}
+
+################################################################
+# START
+################################################################
+
+# To test this script, run the following commands from Vivado Tcl console:
+# source pr_region_2_script.tcl
+
+# If there is no project opened, this script will create a
+# project, but make sure you do not have an existing project
+# <./myproj/project_1.xpr> in the current working folder.
+
+set list_projs [get_projects -quiet]
+if { $list_projs eq "" } {
+   create_project project_1 myproj -part xcvu095-ffvc1517-2-e
+}
+
+
+# CHANGE DESIGN NAME HERE
+set design_name pr_region_2
+
+# If you do not already have an existing IP Integrator design open,
+# you can create a design using the following command:
+#    create_bd_design $design_name
+
+# Creating design if needed
+set errMsg ""
+set nRet 0
+
+set cur_design [current_bd_design -quiet]
+set list_cells [get_bd_cells -quiet]
+
+if { ${design_name} eq "" } {
+   # USE CASES:
+   #    1) Design_name not set
+
+   set errMsg "Please set the variable <design_name> to a non-empty value."
+   set nRet 1
+
+} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
+   # USE CASES:
+   #    2): Current design opened AND is empty AND names same.
+   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
+   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
+
+   if { $cur_design ne $design_name } {
+      common::send_msg_id "BD_TCL-001" "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
+      set design_name [get_property NAME $cur_design]
+   }
+   common::send_msg_id "BD_TCL-002" "INFO" "Constructing design in IPI design <$cur_design>..."
+
+} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
+   # USE CASES:
+   #    5) Current design opened AND has components AND same names.
+
+   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 1
+} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
+   # USE CASES: 
+   #    6) Current opened design, has components, but diff names, design_name exists in project.
+   #    7) No opened design, design_name exists in project.
+
+   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
+   set nRet 2
+
+} else {
+   # USE CASES:
+   #    8) No opened design, design_name not in project.
+   #    9) Current opened design, has components, but diff names, design_name not in project.
+
+   common::send_msg_id "BD_TCL-003" "INFO" "Currently there is no design <$design_name> in project, so creating one..."
+
+   create_bd_design $design_name
+
+   common::send_msg_id "BD_TCL-004" "INFO" "Making design <$design_name> as current_bd_design."
+   current_bd_design $design_name
+
+}
+
+common::send_msg_id "BD_TCL-005" "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
+
+if { $nRet != 0 } {
+   catch {common::send_msg_id "BD_TCL-114" "ERROR" $errMsg}
+   return $nRet
+}
+
+##################################################################
+# DESIGN PROCs
+##################################################################
+
+
+
+# Procedure to create entire design; Provide argument to make
+# procedure reusable. If parentCell is "", will use root.
+proc create_root_design { parentCell } {
+
+  variable script_folder
+
+  if { $parentCell eq "" } {
+     set parentCell [get_bd_cells /]
+  }
+
+  # Get object for parentCell
+  set parentObj [get_bd_cells $parentCell]
+  if { $parentObj == "" } {
+     catch {common::send_msg_id "BD_TCL-100" "ERROR" "Unable to find parent cell <$parentCell>!"}
+     return
+  }
+
+  # Make sure parentObj is hier blk
+  set parentType [get_property TYPE $parentObj]
+  if { $parentType ne "hier" } {
+     catch {common::send_msg_id "BD_TCL-101" "ERROR" "Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."}
+     return
+  }
+
+  # Save current instance; Restore later
+  set oldCurInst [current_bd_instance .]
+
+  # Set parent object as current
+  current_bd_instance $parentObj
+
+
+  # Create interface ports
+  set m_axi [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 m_axi ]
+  set_property -dict [ list \
+CONFIG.ADDR_WIDTH {64} \
+CONFIG.CLK_DOMAIN {static_region_xdma_0_0_axi_aclk} \
+CONFIG.DATA_WIDTH {128} \
+CONFIG.FREQ_HZ {250000000} \
+CONFIG.HAS_BURST {0} \
+CONFIG.HAS_REGION {0} \
+CONFIG.PROTOCOL {AXI4} \
+ ] $m_axi
+  set s_axi [ create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:aximm_rtl:1.0 s_axi ]
+  set_property -dict [ list \
+CONFIG.ADDR_WIDTH {32} \
+CONFIG.ARUSER_WIDTH {0} \
+CONFIG.AWUSER_WIDTH {0} \
+CONFIG.BUSER_WIDTH {0} \
+CONFIG.CLK_DOMAIN {static_region_xdma_0_0_axi_aclk} \
+CONFIG.DATA_WIDTH {32} \
+CONFIG.FREQ_HZ {250000000} \
+CONFIG.HAS_BRESP {1} \
+CONFIG.HAS_BURST {0} \
+CONFIG.HAS_CACHE {0} \
+CONFIG.HAS_LOCK {0} \
+CONFIG.HAS_PROT {1} \
+CONFIG.HAS_QOS {1} \
+CONFIG.HAS_REGION {0} \
+CONFIG.HAS_RRESP {1} \
+CONFIG.HAS_WSTRB {1} \
+CONFIG.ID_WIDTH {0} \
+CONFIG.MAX_BURST_LENGTH {1} \
+CONFIG.NUM_READ_OUTSTANDING {1} \
+CONFIG.NUM_READ_THREADS {1} \
+CONFIG.NUM_WRITE_OUTSTANDING {1} \
+CONFIG.NUM_WRITE_THREADS {1} \
+CONFIG.PROTOCOL {AXI4LITE} \
+CONFIG.READ_WRITE_MODE {READ_WRITE} \
+CONFIG.RUSER_BITS_PER_BYTE {0} \
+CONFIG.RUSER_WIDTH {0} \
+CONFIG.SUPPORTS_NARROW_BURST {0} \
+CONFIG.WUSER_BITS_PER_BYTE {0} \
+CONFIG.WUSER_WIDTH {0} \
+ ] $s_axi
+
+  # Create ports
+  set S00_ARESETN [ create_bd_port -dir I -type rst S00_ARESETN ]
+  set axi_aclk [ create_bd_port -dir I -type clk axi_aclk ]
+  set_property -dict [ list \
+CONFIG.ASSOCIATED_RESET {rst:Res:aresetn:S00_ARESETN} \
+CONFIG.CLK_DOMAIN {static_region_xdma_0_0_axi_aclk} \
+CONFIG.FREQ_HZ {250000000} \
+ ] $axi_aclk
+
+  # Create instance: accelerator_0, and set properties
+  set accelerator_0 [ create_bd_cell -type ip -vlnv xilinx.com:hls:accelerator:1.0 accelerator_0 ]
+  set_property -dict [ list \
+CONFIG.C_M_AXI_MEM_ENABLE_USER_PORTS {false} \
+ ] $accelerator_0
+
+  # Create instance: axi_interconnect_0, and set properties
+  set axi_interconnect_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect_0 ]
+  set_property -dict [ list \
+CONFIG.ENABLE_ADVANCED_OPTIONS {0} \
+CONFIG.M00_HAS_DATA_FIFO {1} \
+CONFIG.M00_HAS_REGSLICE {3} \
+CONFIG.M01_HAS_DATA_FIFO {1} \
+CONFIG.M01_HAS_REGSLICE {3} \
+CONFIG.NUM_MI {1} \
+CONFIG.S00_HAS_DATA_FIFO {1} \
+CONFIG.S00_HAS_REGSLICE {4} \
+ ] $axi_interconnect_0
+
+  # Create instance: axi_interconnect_1, and set properties
+  set axi_interconnect_1 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect_1 ]
+  set_property -dict [ list \
+CONFIG.M00_HAS_DATA_FIFO {1} \
+CONFIG.M00_HAS_REGSLICE {4} \
+CONFIG.NUM_MI {1} \
+CONFIG.NUM_SI {2} \
+CONFIG.S00_HAS_DATA_FIFO {1} \
+CONFIG.S00_HAS_REGSLICE {3} \
+CONFIG.S01_HAS_DATA_FIFO {1} \
+CONFIG.S01_HAS_REGSLICE {3} \
+ ] $axi_interconnect_1
+
+  # Create instance: clk_wiz_0, and set properties
+  set clk_wiz_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:5.4 clk_wiz_0 ]
+  set_property -dict [ list \
+CONFIG.CLKIN1_JITTER_PS {40.0} \
+CONFIG.CLKOUT1_DRIVES {Buffer} \
+CONFIG.CLKOUT1_JITTER {130.067} \
+CONFIG.CLKOUT1_MATCHED_ROUTING {true} \
+CONFIG.CLKOUT1_PHASE_ERROR {122.096} \
+CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {125.00} \
+CONFIG.CLKOUT2_DRIVES {Buffer} \
+CONFIG.CLKOUT3_DRIVES {Buffer} \
+CONFIG.CLKOUT4_DRIVES {Buffer} \
+CONFIG.CLKOUT5_DRIVES {Buffer} \
+CONFIG.CLKOUT6_DRIVES {Buffer} \
+CONFIG.CLKOUT7_DRIVES {Buffer} \
+CONFIG.ENABLE_CLOCK_MONITOR {false} \
+CONFIG.MMCM_CLKFBOUT_MULT_F {5} \
+CONFIG.MMCM_CLKIN1_PERIOD {4.000} \
+CONFIG.MMCM_CLKIN2_PERIOD {10.0} \
+CONFIG.MMCM_CLKOUT0_DIVIDE_F {5} \
+CONFIG.MMCM_COMPENSATION {AUTO} \
+CONFIG.MMCM_DIVCLK_DIVIDE {2} \
+CONFIG.PRIMITIVE {PLL} \
+CONFIG.PRIM_IN_FREQ {250.00} \
+CONFIG.PRIM_SOURCE {No_buffer} \
+CONFIG.RESET_PORT {resetn} \
+CONFIG.RESET_TYPE {ACTIVE_LOW} \
+CONFIG.SECONDARY_SOURCE {Single_ended_clock_capable_pin} \
+CONFIG.USE_PHASE_ALIGNMENT {false} \
+CONFIG.USE_RESET {true} \
+ ] $clk_wiz_0
+
+  # Create instance: pcl_axifull_bridge_0, and set properties
+  set pcl_axifull_bridge_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:pcl_axifull_bridge:1.0 pcl_axifull_bridge_0 ]
+  set_property -dict [ list \
+CONFIG.ADDR_WIDTH {64} \
+CONFIG.DATA_WIDTH {128} \
+ ] $pcl_axifull_bridge_0
+
+  # Create instance: pcl_axilite_bridge_0, and set properties
+  set pcl_axilite_bridge_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:pcl_axilite_bridge:1.0 pcl_axilite_bridge_0 ]
+  set_property -dict [ list \
+CONFIG.ADDR_WIDTH {19} \
+CONFIG.DATA_WIDTH {32} \
+ ] $pcl_axilite_bridge_0
+
+  # Create instance: proc_sys_reset_0, and set properties
+  set proc_sys_reset_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset_0 ]
+
+  # Create interface connections
+  connect_bd_intf_net -intf_net accelerator_0_m_axi_mem [get_bd_intf_pins accelerator_0/m_axi_mem] [get_bd_intf_pins axi_interconnect_1/S00_AXI]
+  connect_bd_intf_net -intf_net axi_interconnect_0_M00_AXI [get_bd_intf_pins accelerator_0/s_axi_CTRL_BUS] [get_bd_intf_pins axi_interconnect_0/M00_AXI]
+  connect_bd_intf_net -intf_net axi_interconnect_1_M00_AXI [get_bd_intf_pins axi_interconnect_1/M00_AXI] [get_bd_intf_pins pcl_axifull_bridge_0/s_axi]
+  connect_bd_intf_net -intf_net pcl_axifull_bridge_0_m_axi [get_bd_intf_ports m_axi] [get_bd_intf_pins pcl_axifull_bridge_0/m_axi]
+  connect_bd_intf_net -intf_net pcl_axilite_bridge_0_m_axi [get_bd_intf_pins axi_interconnect_0/S00_AXI] [get_bd_intf_pins pcl_axilite_bridge_0/m_axi]
+  connect_bd_intf_net -intf_net s_axi_1 [get_bd_intf_ports s_axi] [get_bd_intf_pins pcl_axilite_bridge_0/s_axi]
+
+  # Create port connections
+  connect_bd_net -net S00_ARESETN_1 [get_bd_ports S00_ARESETN] [get_bd_pins axi_interconnect_0/ARESETN] [get_bd_pins axi_interconnect_0/S00_ARESETN] [get_bd_pins axi_interconnect_1/ARESETN] [get_bd_pins axi_interconnect_1/M00_ARESETN] [get_bd_pins clk_wiz_0/resetn] [get_bd_pins pcl_axifull_bridge_0/aresetn] [get_bd_pins pcl_axilite_bridge_0/aresetn] [get_bd_pins proc_sys_reset_0/ext_reset_in]
+  connect_bd_net -net axi_aclk_1 [get_bd_ports axi_aclk] [get_bd_pins axi_interconnect_0/ACLK] [get_bd_pins axi_interconnect_0/S00_ACLK] [get_bd_pins axi_interconnect_1/ACLK] [get_bd_pins axi_interconnect_1/M00_ACLK] [get_bd_pins clk_wiz_0/clk_in1] [get_bd_pins pcl_axifull_bridge_0/aclk] [get_bd_pins pcl_axilite_bridge_0/aclk]
+  connect_bd_net -net clk_wiz_0_clk_out1 [get_bd_pins accelerator_0/ap_clk] [get_bd_pins axi_interconnect_0/M00_ACLK] [get_bd_pins axi_interconnect_1/S00_ACLK] [get_bd_pins axi_interconnect_1/S01_ACLK] [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins proc_sys_reset_0/slowest_sync_clk]
+  connect_bd_net -net proc_sys_reset_0_interconnect_aresetn [get_bd_pins axi_interconnect_0/M00_ARESETN] [get_bd_pins axi_interconnect_1/S00_ARESETN] [get_bd_pins axi_interconnect_1/S01_ARESETN] [get_bd_pins proc_sys_reset_0/interconnect_aresetn]
+  connect_bd_net -net proc_sys_reset_0_peripheral_aresetn [get_bd_pins accelerator_0/ap_rst_n] [get_bd_pins proc_sys_reset_0/peripheral_aresetn]
+
+  # Create address segments
+  create_bd_addr_seg -range 0x000100000000 -offset 0x00000000 [get_bd_addr_spaces accelerator_0/Data_m_axi_mem] [get_bd_addr_segs m_axi/Reg] SEG_m_axi_Reg
+  create_bd_addr_seg -range 0x00010000 -offset 0x00000000 [get_bd_addr_spaces s_axi] [get_bd_addr_segs accelerator_0/s_axi_CTRL_BUS/Reg] SEG_accelerator_0_Reg
+
+
+  # Restore current instance
+  current_bd_instance $oldCurInst
+
+  save_bd_design
+}
+# End of create_root_design()
+
+
+##################################################################
+# MAIN FLOW
+##################################################################
+
+create_root_design ""
+
+
---
+++ script_template/create_pr_accelerator.tcl
@@ -0,0 +1,13 @@
+#CALL THIS SCRIPT FROM THE PARENT FOLDER!!!!
+#Set project name -- Change for other projects!!!!
+set projName accelerator_pr
+
+#Open the project, regardless of whether it exists
+create_project -f pr_region 8v3_shell/$projName -part xcvu095-ffvc1517-2-e
+#Add project ip directories. Change for other projects!
+set_property  ip_repo_paths  {8v3_shell/ocl_ips hls_proj/accelerator/solution_partitionAndParallel/impl/ip} [current_project]
+update_ip_catalog
+
+#source pr_region_2_bd.tcl
+#source 8v3_shell/nn_bd.tcl
+source accelerator_bd.tcl
