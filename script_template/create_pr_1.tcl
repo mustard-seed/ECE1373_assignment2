90ddd1073f454372ef17d3b3f6647ec3a84728fc
James Liu <james2006roger@ayoo.ca>
2018-03-17 00:08:16 -0400

    Modified create_pr_1.tcl to reset runs

--- script_template/create_pr_1.tcl
+++ script_template/create_pr_1.tcl
@@ -6,6 +6,9 @@ update_compile_order -fileset sources_1
 generate_target {synthesis implementation} [get_files 8v3_shell/$projName/pr_region.srcs/sources_1/bd/pr_region_2/pr_region_2.bd]
 export_ip_user_files -of_objects [get_files 8v3_shell/$projName/pr_region.srcs/sources_1/bd/pr_region_2/pr_region_2.bd] -no_script -sync -force -quiet
 create_ip_run [get_files -of_objects [get_fileset sources_1] 8v3_shell/$projName/pr_region.srcs/sources_1/bd/pr_region_2/pr_region_2.bd]
+foreach run_name [get_runs *_synth*] {
+  reset_run ${run_name}
+}
 launch_runs [get_runs *_synth*] -jobs 12
 foreach run_name [get_runs *_synth*] {
   wait_on_run ${run_name}
