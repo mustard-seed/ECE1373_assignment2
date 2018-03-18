2ebbf0917a6b48be3dd87db34aed2b7a39aa170f
James <james2006roger@yahoo.ca>
2018-03-18 01:00:08 +0000

    Start to work on first test SW!

--- script_template/gen_pr.tcl
+++ script_template/gen_pr.tcl
@@ -12,4 +12,5 @@ write_checkpoint -force 8v3_shell/${projName}_routed.dcp
 #write_checkpoint -cell static_region_i/pr_region rp2_route_design.dcp
 write_bitstream -force 8v3_shell/$projName.bit 
 report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -name timing_1 -file 8v3_shell/$projName.timing
-
+current_project project_static_routed_v2
+close_design
